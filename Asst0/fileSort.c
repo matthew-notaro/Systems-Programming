9a5965362cb19819415d77a1337c31aeda6d9b78
Matthew Notaro <matthew.y.notaro@gmail.com>
2020-02-25 21:11:09 -0500

    2

--- Asst0/fileSort.c
+++ Asst0/fileSort.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 
-int insertionSort(void* toSort, int (*comparator)(void*, void*)   )
-int quickSort(void* toSort, int (*comparator)(void*, void*) )
+int insertionSort(void* toSort, int (*comparator)(void*, void*));
+int quickSort(void* toSort, int (*comparator)(void*, void*));
 int main(int argc, char** argv){
     // Command Line: exec file, sorting algo (-i / -q), file name
     if(argc != 3){
@@ -14,7 +14,7 @@ int main(int argc, char** argv){
     char* algo = argv[1];
     char* file = argv[2];
 
-    int fd = open(file, 0_RDONLY);
+    int fd = open(file, "O_RDONLY");
 
 
     return 0;
