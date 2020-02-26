316671ce6748c29b56d0d51474a6b413540a38cf
Matthew Notaro <matthew.y.notaro@gmail.com>
2020-02-25 20:47:39 -0500

    test

---
+++ Asst0/fileSort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int insertionSort(void* toSort, int (*comparator)(void*, void*)   )
+int quickSort(void* toSort, int (*comparator)(void*, void*) )
+i
+nt main(int argc, char** argv){
+    // Command Line: exec file, sorting algo (-i / -q), file name
+    if(argc != 3){
+        return -1;
+    }
+    char* algo = argv[1];
+    char* file = argv[2];
+
+
+    return 0;
+}
