cf09b4913a98ff9f2892852474f7968a83efad86
Samantha Lee <sll155@rutgers.edu>
2020-02-25 21:19:25 -0500

    Update fileSort.c
    i hate this

--- Asst0/fileSort.c
+++ Asst0/fileSort.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int insertionSort(void* toSort, int (*comparator)(void*, void*));
 int quickSort(void* toSort, int (*comparator)(void*, void*));
+void init_things(char* file);
+
 int main(int argc, char** argv){
     // Command Line: exec file, sorting algo (-i / -q), file name
     if(argc != 3){
@@ -14,8 +15,18 @@ int main(int argc, char** argv){
     char* algo = argv[1];
     char* file = argv[2];
 
-    int fd = open(file, "O_RDONLY");
-
+    init_things(file);
 
     return 0;
+}
+
+void init_things(char* file)
+{
+  int fd = open(file, "O_RDONLY");
+  struct stat *buffer = malloc(sizeof(struct stat);
+  stat(file, buffer);
+  int size = buffer->st_size;
+
+
+
 }
