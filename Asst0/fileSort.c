bfc82c0f6da1dc16d3d5452b25a3d03b6b6beb8f
Samantha Lee <sll155@rutgers.edu>
2020-02-28 15:23:36 -0500

    update

--- Asst0/fileSort.c
+++ Asst0/fileSort.c
@@ -13,37 +13,48 @@ typedef struct Node
   struct Node* next;
 } Node;
 
-int insertionSort(void* toSort, int (*comparator)(void*, void*));
-int quickSort(void* toSort, int (*comparator)(void*, void*));
-char* init_things(char* file);
-void extract_tokens(char* file_string);
-Node* insert(char* num, Node* head);
+int stringCmp(void*, void*);
+int intCmp(void*, void*);
+
+int insertionSort(void*, int (*comparator)(void*, void*));
+int quickSort(void*, int (*comparator)(void*, void*));
+
+char* initThings(char* file);
+void extractTokens(char* file_string);
+Node* insert(void* num, Node* head);
+
+void printLL(struct Node* head); // Useful function to print out LL
+
+
+// Keep track of number of nodes in linked list
+int size = 0;
 
 int main(int argc, char** argv){
     // Command Line: exec file, sorting algo (-i / -q), file name
-    if(argc != 3) {
-      return -1;
+    if(argc != 3){
+        return -1;
     }
+
+    //if no file
 
     char* algo = argv[1];
     char* file = argv[2];
 
-    char* file_string = init_things(file);
-    //printf("my file's content is %s\n", file_string);
-    extract_tokens(file_string);
+    char* file_string = initThings(file);
+    extractTokens(file_string);
 
     return 0;
 }
 
-char* init_things(char* file)
+//Read entire file into string buffer
+char* initThings(char* file)
 {
   int fd = open(file, O_RDONLY);
   struct stat *buffer = malloc(sizeof(struct stat));
   stat(file, buffer);
   int size = buffer->st_size;
-  printf("size %d\n", size);
 
-  //if empty file
+  //if empty file do something
 
   char* file_buffer = malloc(size);
   memset(file_buffer, '\0', size);
@@ -56,65 +67,156 @@ char* init_things(char* file)
       //printf("readIn: %d\n", readIn);
     } while(status > 0 && readIn < size);
 
-  return file_buffer; //check --> warning?
+  return file_buffer;
 }
 
-Node* insert(char* token, Node* head)
-{
-  //case: insert to empty list
-  if(head == NULL)
-  {
-    struct Node* newNode = (Node *)malloc(sizeof(Node));
-    newNode->data = token;
-    newNode->next = NULL;
-    newNode->prev = NULL;
-    head = newNode;
-    return head;
-  }
-
-  //case: insert normally
-  struct Node* newNode = (Node *)malloc(sizeof(Node));
-  newNode->data = token;
-  newNode->next = head;
-  head->prev = newNode;
-  head = newNode;
-  return head;
-}
-
-void extract_tokens(char* file_string)
+//Extract tokens from file string
+void extractTokens(char* file_string)
 {
   struct Node* head = (Node *)malloc(sizeof(Node));
-  int start = 0;
   int len = strlen(file_string);
+  int start = 0;
   int i = 0;
-
-  printf("extracting\n");
+  int j = 0;
 
   for(i = 0; i < len; i++)
   {
-    if(file_string[i] == ',')
+    //Token found; extract
+    if(file_string[i] == ',' || (i == len-1 && start < len-1))
     {
-      //token found; create node
-      printf("comma found\n");
-      char substring[i-start+1];
-      strncpy(substring, file_string+start, i-start);
-      substring[i-start] = '\0';
-      printf("substring: %s\n", substring);
+      //Malloc space to hold substr from start to location of comma, +1 for '\0'
+      char* substring = (char*)malloc(i-start+1);
 
+      memset(substring, '\0', i-start+1);
+
+      //Loop though chars of substring, find non-white spaces
+      int substr_cnt = 0;
+      for(j = start; j < i; j++)
+      {
+        if(file_string[j] != ' ')
+        {
+          substring[substr_cnt] = file_string[j];
+          substr_cnt++;
+        }
+      }
+
+      //Insert into linked list
       head = insert(substring, head);
 
+      //Increment starting point for next token
       if(i+1 < len)
         start = i+1;
+
+      free(substring);
     }
   }
+}
 
-  //get last item
-  if(start < len)
-  {
-    char substring[len-start];
-    strncpy(substring, file_string+start, len-1-start);
-    substring[len-1-start] = '\0';
-    printf("substring: %s\n", substring);
-    head = insert(substring, head);
+// SORTS
+// Implementation of insertion sort on LL
+// Returns -1 on NULL LL, 0 on success
+int insertionSort(void* toSort, int (*comparator)(void*, void*)){
+    if(toSort == NULL){
+        return -1;
+    }
+    Node* front = (Node*)toSort;
+    Node* currNode;
+    // Iterates through for every element in LL
+    for(currNode = front; currNode != NULL; currNode = currNode->next){
+        Node* ptr = currNode;                   // Creates ptr to compare previous elements
+        void* currData = currNode->data;        // Saves currNode data so it's not lost when overwritten
+        // Compares previous values to curr, if prev>curr then shift prev value right by one and continue
+        while(ptr->prev != NULL && (*comparator)(ptr->prev->data, currData) > 0){
+            ptr->data = ptr->prev->data;
+            ptr = ptr->prev;
+        }
+        // Sets currData in appropriate order
+        ptr->data = currData;
+        printLL(front);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// Implementation of quick sort on LL
+// Returns -1 on NULL LL, 0 on success
+int quickSort(void* toSort, int (*comparator)(void*, void*)){
+    if(toSort == NULL){
+        return -1;
+    }
+    Node* front = (Node*)toSort;
+    Node* currNode;
+
+    // Iterates through for every element in LL
+    for(currNode = front; currNode != NULL; currNode = currNode->next){
+        Node* ptr = currNode;                   // Creates ptr to compare previous elements
+        void* currData = currNode->data;        // Saves currNode data so it's not lost when overwritten
+        // Compares previous values to curr, if prev>curr then shift prev value right by one and continue
+        while(ptr->prev != NULL && (*comparator)(ptr->prev->data, currData) > 0){
+            ptr->data = ptr->prev->data;
+            ptr = ptr->prev;
+        }
+        // Sets currData in appropriate order
+        ptr->data = currData;
+        printLL(front);
+    }
+
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+// Returns null on failed malloc, head of new LL otherwise
+Node* insert(void* token, Node* head){
+  struct Node* newNode = (Node *)malloc(sizeof(Node));
+  if(newNode == NULL){
+      printf("Bad malloc\n");
+      return NULL;
   }
+  newNode->data = token;
+  newNode->next = head;
+  newNode->prev = NULL;
+  //case: linked old head's prev if it exists to new head
+  if(head != NULL){
+    head->prev = newNode;
+  }
+  return newNode;
+}
+
+
+
+
+//COMPARATORS
+// Returns <0 if t1<t2, 0 if t1=t2, >0 if t1>t2
+int stringCmp(void* thing1, void* thing2){
+    char *string1 = (char*)thing1, *string2 = (char*)thing2;
+    int i = 0;
+    while((string1[i] != '\0') && (string1[i] == string2[i]))
+        i++;
+    return string1[i] - string2 [i];
+}
+// Returns <0 if t1<t2, 0 if t1=t2, >0 if t1>t2
+int intCmp(void* thing1, void* thing2){
+    return *(int*)thing1 - *(int*)thing2;
+}
+
+
+// Useful little function
+void printLL(Node* head){
+    while(head != NULL){
+        printf("%s ", (char*)head->data);
+        head = head->next;
+    }
+    printf("\n");
 }
