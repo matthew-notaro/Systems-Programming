1f5724b1b32d7bde3327811eafa586c352247029
Matthew Notaro <matthew.y.notaro@gmail.com>
2020-02-29 02:08:10 -0500

    Added file type check, some warnings/errors, fixed return types, added main

--- Asst0/fileSort.c
+++ Asst0/fileSort.c
@@ -5,10 +5,11 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <ctype.h>
 
 typedef struct Node
 {
-  char* data;
+  void* data;
   struct Node* prev;
   struct Node* next;
 } Node;
@@ -19,8 +20,8 @@ int intCmp(void*, void*);
 int insertionSort(void*, int (*comparator)(void*, void*));
 int quickSort(void*, int (*comparator)(void*, void*));
 
-char* initThings(char* file);
-void extractTokens(char* file_string);
+char* readFromFile(char* file);
+Node* extractAndBuild(char* file_string);
 Node* insert(void* num, Node* head);
 
 void printLL(struct Node* head); // Useful function to print out LL
@@ -28,6 +29,7 @@ void printLL(struct Node* head); // Useful function to print out LL
 
 // Keep track of number of nodes in linked list
 int size = 0;
+int isIntFile = 0;
 
 int main(int argc, char** argv){
     // Command Line: exec file, sorting algo (-i / -q), file name
@@ -35,82 +37,112 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    //if no file
-
-    char* algo = argv[1];
+    char* sortingAlgo = argv[1];
     char* file = argv[2];
 
-    char* file_string = initThings(file);
-    extractTokens(file_string);
+    // Reads
+    char* file_string = readFromFile(file);
+    Node* front = extractAndBuild(file_string);
 
-    return 0;
-}
-
-//Read entire file into string buffer
-char* initThings(char* file)
-{
-  int fd = open(file, O_RDONLY);
-  struct stat *buffer = malloc(sizeof(struct stat));
-  stat(file, buffer);
-  int size = buffer->st_size;
-
-  //if empty file do something
-
-  char* file_buffer = malloc(size);
-  memset(file_buffer, '\0', size);
-  int status = 1;
-  int readIn = 0;
-  do{
-      status = read(fd, file_buffer+readIn, size-readIn);
-      //printf("status: %d\n", status);
-      readIn += status;
-      //printf("readIn: %d\n", readIn);
-    } while(status > 0 && readIn < size);
-
-  return file_buffer;
-}
-
-//Extract tokens from file string
-void extractTokens(char* file_string)
-{
-  struct Node* head = (Node *)malloc(sizeof(Node));
-  int len = strlen(file_string);
-  int start = 0;
-  int i = 0;
-  int j = 0;
-
-  for(i = 0; i < len; i++)
-  {
-    //Token found; extract
-    if(file_string[i] == ',' || (i == len-1 && start < len-1))
-    {
-      //Malloc space to hold substr from start to location of comma, +1 for '\0'
-      char* substring = (char*)malloc(i-start+1);
-
-      memset(substring, '\0', i-start+1);
-
-      //Loop though chars of substring, find non-white spaces
-      int substr_cnt = 0;
-      for(j = start; j < i; j++)
-      {
-        if(file_string[j] != ' ')
-        {
-          substring[substr_cnt] = file_string[j];
-          substr_cnt++;
-        }
-      }
-
-      //Insert into linked list
-      head = insert(substring, head);
-
-      //Increment starting point for next token
-      if(i+1 < len)
-        start = i+1;
-
-      free(substring);
+    int sortSuccessful = 0;
+    if(stringCmp(sortingAlgo, "-i")){
+        if(isIntFile)
+            sortSuccessful = insertionSort(head, intCmp);
+        else 
+            sortSuccessful = insertionSort(head, stringCmp);
     }
-  }
+    else{
+        if(isIntFile)
+            sortSuccessful = quickSort(head, intCmp);
+        else 
+            sortSuccessful = quickSort(head, stringCmp);
+    }
+    return sortSuccessful;
+    /* Can replace 14 previous lines with following 1 statement
+    return = (stringCmp(sortingAlgo, "-i") == 0) ? 
+        (isIntFile ? insertionSort(head, intCmp) : insertionSort(head, stringCmp)) :
+        (isIntFile ? quickSort(head, intCmp) : quickSort(head, stringCmp));
+    */
 }
+
+// Read entire file into string buffer
+// Returns NULL if file does not exist, string otherwise
+char* readFromFile(char* file)
+{
+    int fd = open(file, O_RDONLY);    // Returns -1 on failure, >0 on success
+    // Fatal Error if file does not exist
+    if(fd < 0){
+        printf("Fatal Error: File does not exist.\n");
+        return NULL;
+    }
+    struct stat *buffer = malloc(sizeof(struct stat));
+    stat(file, buffer);
+    int size = buffer->st_size;
+    // Warning: Empty file
+    if(size == 0){
+        printf("Warning: Empty file.\n");
+    }
+    // IO Read Loop
+    char* file_buffer = (char*)malloc(size);
+    memset(file_buffer, '\0', size);
+    int status = 1;
+    int readIn = 0;
+    do{
+        status = read(fd, file_buffer+readIn, size-readIn);
+        //printf("status: %d\n", status);
+        readIn += status;
+        //printf("readIn: %d\n", readIn);
+    } while(status > 0 && readIn < size);
+    
+    return file_buffer;
+}
+
+// Extract tokens from file string
+// Returns head of linked list
+Node* extractAndBuild(char* file_string)
+{
+    Node* head = (Node*)malloc(sizeof(Node));
+    int len = strlen(file_string);
+    int start = 0, i = 0, j = 0;
+
+    // Checks if dealing with a string or int file
+    for(i = 0; i < len; i++){
+        if(isdigit(file_string[i])){
+            isIntFile = 1;
+        }
+        else if(isalpha(file_string)){
+            isIntFile = 0;
+        }
+    }
+
+    // Iterates through file_string
+    for(i = 0; i < len; i++){
+        //Token found; extract
+        if(file_string[i] == ',' || (i == len-1 && start < len-1)){
+            //Malloc space to hold substr from start to location of comma, +1 for '\0'
+            char* substring = (char*)malloc(i-start+1);
+            memset(substring, '\0', i-start+1);
+            //Loop though chars of substring, find non-white spaces
+            int substr_cnt = 0;
+            for(j = start; j < i; j++){
+                if(file_string[j] != ' ' && file_string[j] != '\n' && file_string[j] != '\t'){
+                    substring[substr_cnt] = file_string[j];
+                    substr_cnt++;
+                }
+            }
+            //Insert into linked list
+            head = insert(substring, head);
+            //Increment starting point for next token
+            if(i+1 < len)
+                start = i+1;
+
+            free(substring);
+        }
+    }
+    free(file_string);
+    return head;
+}
+
 
 // SORTS
 // Implementation of insertion sort on LL
@@ -136,11 +168,6 @@ int insertionSort(void* toSort, int (*comparator)(void*, void*)){
     }
     return 0;
 }
-
-
-
-
-
 
 
 // Implementation of quick sort on LL
@@ -169,11 +196,6 @@ int quickSort(void* toSort, int (*comparator)(void*, void*)){
     
     return 0;
 }
-
-
-
-
-
 
 
 
