e5ad03d5862e662f5c61b4bf01e4d241c48ae7ac
Samantha Lee <sll155@rutgers.edu>
2020-04-01 01:15:10 -0400

    various changes
    post-submissions

--- Asst0/fileSort.c
+++ Asst0/fileSort.c
@@ -14,11 +14,11 @@ typedef struct Node
   struct Node* next;
 } Node;
 
-int stringCmp(void*, void*);
-int intCmp(void*, void*);
+int stringCmp(void* thing1, void* thing2);
+int intCmp(void* thing1, void* thing2);
 
-int insertionSort(void*, int (*comparator)(void*, void*));
-int quickSort(void*, int (*comparator)(void*, void*));
+int insertionSort(void* head, int (*comparator)(void*, void*));
+int quickSort(void* head, int (*comparator)(void*, void*));
 
 Node* recursiveQuickSort(Node* left, Node* right, int (*comparator)(void*, void*));
 Node* partition(Node* left, Node* right, int (*comparator)(void*, void*));
@@ -27,9 +27,9 @@ char* readFromFile(char* file);
 Node* extractAndBuild(char* file_string);
 Node* insert(void* num, Node* head);
 
-void freeLL(Node*);
+void freeLL(Node* head);
 
-void printLL(struct Node* head); // Useful function to print out LL
+void printLL(Node* head); // Useful function to print out LL
 
 // Keep track of number of nodes in linked list
 int size = 0;
@@ -38,27 +38,41 @@ int isIntFile = 0;
 int main(int argc, char** argv){
     // Command Line: exec file, sorting algo (-i / -q), file name
     if(argc != 3){
-        printf("Enter correct parameters\n");
+        printf("Fatal Error: expected two arguments, had one\n");
         return -1;
     }
 
 
     char* sortingAlgo = argv[1];
+
+
     char* file = argv[2];
+
+    // Checks if sorting algo flag is correct
+    if((stringCmp(sortingAlgo, "-i") != 0) && (stringCmp(sortingAlgo, "-q") != 0)){
+        printf("Fatal Error: \"%s\" is not a valid sort flag\n", sortingAlgo);
+        return -1;
+    }
+
 
     // Reads
     char* file_string = readFromFile(file);
+    // Checks for nonexistent file
+    if(file_string == NULL){
+        printf("Fatal Error: file \"%s\" does not exist", file);
+        return -1;
+    }
+
     Node* head = extractAndBuild(file_string);
 
     // Sorts
-    int status = (stringCmp(sortingAlgo, "-i") == 0) ?
+    int status= (stringCmp(sortingAlgo, "-i") == 0) ?
         (isIntFile ? insertionSort(head, intCmp) : insertionSort(head, stringCmp)) :
-        ((stringCmp(sortingAlgo, "-q") == 0) ?
-        (isIntFile ? quickSort(head, intCmp) : quickSort(head, stringCmp)) :
-        -1);
+        (isIntFile ? quickSort(head, intCmp) : quickSort(head, stringCmp));
 
+    //printf("intFile: %d\n", isIntFile);
+    printLL(head);
     freeLL(head);
-
     return status;
 }
 
@@ -78,26 +92,26 @@ char* readFromFile(char* file)
         return NULL;
     }
     stat(file, buffer);
-    int size = buffer->st_size;
+    int buffer_size = buffer->st_size;
     // Warning: Empty file
-    if(size == 0){
+    if(buffer_size == 0){
         printf("Warning: Empty file.\n");
     }
     // IO Read Loop
-    char* file_buffer = (char*)malloc(size);
+    char* file_buffer = (char*)malloc(buffer_size);
     if(file_buffer == NULL){
         printf("Bad malloc\n");
         return NULL;
     }
-    memset(file_buffer, '\0', size);
+    memset(file_buffer, '\0', buffer_size);
     int status = 1;
     int readIn = 0;
     do{
-        status = read(fd, file_buffer+readIn, size-readIn);
+        status = read(fd, file_buffer+readIn, buffer_size - readIn);
         //printf("status: %d\n", status);
         readIn += status;
         //printf("readIn: %d\n", readIn);
-    } while(status > 0 && readIn < size);
+    } while(status > 0 && readIn < buffer_size);
 
     free(buffer);
     return file_buffer;
@@ -109,22 +123,39 @@ Node* extractAndBuild(char* file_string)
 {
     Node* head = NULL;
     int len = strlen(file_string);
-    int start = 0, i = 0, j = 0;
+    int start = 0, i = 0, j = 0, k = 0;
 
     // Checks if dealing with a string or int file
-    for(i = 0; i < len; i++){
+    while(i < len && i != ',')
+    {
         if(isdigit(file_string[i])){
             isIntFile = 1;
+            break;
         }
         else if(isalpha(file_string[i])){
             isIntFile = 0;
+            break;
         }
+        i++;
+    }
+
+    int emptyFile = 1;
+    for(k = 0; k < len; k++){
+        if(isspace(file_string[k]) == 0)
+        {
+            emptyFile = 0;
+            break;
+        }
+    }
+
+    if(emptyFile == 1){
+      printf("Warning: Empty File.\n");
     }
 
     // Iterates through file_string
     for(i = 0; i < len; i++){
         //Token found; extract
-        if(file_string[i] == ',' || (i == len-1 && start < len-1)){
+        if(file_string[i] == ','){
             //Malloc space to hold substr from start to location of comma, +1 for '\0'
             char* substring = (char*)malloc(i-start+1);
             if(substring == NULL){
@@ -141,6 +172,30 @@ Node* extractAndBuild(char* file_string)
                 }
             }
             //Inserts into linked list
+            //printf("try to insert: %s:\n", substring);
+            head = insert(substring, head);
+            //Increments starting point for next token
+            if(i+1 < len)
+                start = i+1;
+        }
+        if(i == len-1 && start < len-1){
+            //Malloc space to hold substr from start to location of comma, +1 for '\0'
+            char* substring = (char*)malloc(i-start+1);
+            if(substring == NULL){
+                printf("Bad malloc\n");
+                return NULL;
+            }
+            memset(substring, '\0', i-start+1);
+            //Loops though chars of substring, find non-white spaces
+            int substr_cnt = 0;
+            for(j = start; j <= i; j++){
+                if(!isspace(file_string[j])){
+                    substring[substr_cnt] = file_string[j];
+                    substr_cnt++;
+                }
+            }
+            //Inserts into linked list
+            //printf("try to insert: %s:\n", substring);
             head = insert(substring, head);
             //Increments starting point for next token
             if(i+1 < len)
@@ -150,6 +205,7 @@ Node* extractAndBuild(char* file_string)
     free(file_string);
     return head;
 }
+
 
 
 // SORTS
@@ -172,7 +228,6 @@ int insertionSort(void* toSort, int (*comparator)(void*, void*)){
         }
         // Sets currData in appropriate order
         ptr->data = currData;
-        printLL(head);
     }
     return 0;
 }
@@ -191,10 +246,7 @@ int quickSort(void* toSort, int (*comparator)(void*, void*)){
     {
       rear = rear->next;
     }
-
     head = recursiveQuickSort(head, rear, comparator);
-    printLL(head);
-
     return 0;
 }
 
@@ -326,20 +378,23 @@ int stringCmp(void* thing1, void* thing2){
 }
 // Returns <0 if t1<t2, 0 if t1=t2, >0 if t1>t2
 int intCmp(void* thing1, void* thing2){
-    return *(int*)thing1 - *(int*)thing2;
+    return atoi((char*)thing1) - atoi((char*)thing2);
 }
 
 
 // Useful little function
 void printLL(Node* head){
     while(head != NULL){
-        if(!isIntFile)
-          printf("%s\n", (char*)head->data);
-        else
-          printf("%d\n", *((int*)head->data));
+        if(isIntFile){
+            printf("%d\n", atoi(head->data));
+        }
+        else{
+            printf("%s\n", (char*)head->data);
+        }
         head = head->next;
     }
 }
+
 
 void freeLL(Node* head){
 	Node *prev;
