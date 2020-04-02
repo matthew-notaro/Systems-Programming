e7b27b106c6ec32d4b4191d05b46519fe499e6d7
Matthew Notaro <matthew.y.notaro@gmail.com>
2020-04-01 23:25:57 -0400

    Added command-line interface

--- Asst1/fileCompressor.c
+++ Asst1/fileCompressor.c
@@ -19,12 +19,45 @@ void decompress();
 void recurse(DIR* dd);
 void read_file(char* file);
 
-int b_flag = 0;
-int c_flag = 0;
-int d_flag = 0;
+char opFlag = '?';
+int r_flag = 0;
+char* path;
+char* codebook;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
+  // min args - ./file -b <path>, max args - ./file -R -d <path> <codebook>
+  if(argc < 3 || argc > 5){
+      return -1;
+  }
+  int i;
+  for(i = 1; i < argc - 1; i++){
+    if(strlen(argv[i]) == 2 && argv[i][0] == '-'){ // found a flag
+      char flag = argv[i][1];
+      if(flag == 'R' && !r_flag){ // flag is R and has not been set yet
+        r_flag = 1;
+      }
+      else if((flag == 'b' || flag == 'c' || flag == 'd') && opFlag == '?'){ // flag is op and has not been set yet
+        opFlag = flag;
+        if(flag == 'b'){
+          path = argv[argc-1];
+        }
+        else{
+          path = argv[argc-2];
+          codebook = argv[argc-1];
+        }
+      }
+      else{
+        printf("Invalid flag\n");
+        return -1;
+      }
+    }
+    else if(opFlag == '?'){
+      printf("Operation not specified\n");
+      return -1;
+    }
+  }
+
+  
 
   DIR* dd = opendir("./");
 
