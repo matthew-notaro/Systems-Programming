e07b7678adf5a16b1a98a9d15e7dfe38dcd03b2a
Matthew Notaro <matthew.y.notaro@gmail.com>
2020-04-03 23:14:37 -0400

    Huff stuff

--- Asst1/huff.c
+++ Asst1/huff.c
@@ -9,15 +9,19 @@ void huffEncode(heapNode** heap){
     while(getHeapSize() != 1){ // guaranteed at least 2 elements in heap
         min1 = deleteMin(heap);
         min2 = deleteMin(heap);
+        printf("deleted: %s, %s\n", min1->root->token, min2->root->token);
         min1->freq += min2->freq;
         BSTNode* temp = min1->root;
         min1->root = (BSTNode*)malloc(sizeof(BSTNode));
-        min1->root->token = min1->freq; // need to figure out how to convert int to string
+        min1->root->token = "freq"; //+ min1->freq; // need to figure out how to convert int to string
         min1->root->left = temp;
         min1->root->right = min2->root;
         insertHeap(heap, min1);
         free(min2);
     }
-    printf("total freq: %d\n", min1->freq);
-    printBST(min1->root);
+}
+
+void printHuff(heapNode* huffTree){
+    printf("total freq: %d\n", huffTree->freq);
+    printBST(huffTree->root);
 }
