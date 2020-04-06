0f9915904b7814724009491c14d8a9a4591abc5b
Samantha Lee <35175439+SamanthaLLee@users.noreply.github.com>
2020-04-06 03:19:49 -0400

    Update fileCompressor.c

--- Asst1/fileCompressor.c
+++ Asst1/fileCompressor.c
@@ -24,74 +24,7 @@ char opFlag = '?';
 char* path;
 char* codebook;
 BSTNode* finalBST = NULL;
-/*
-// test main for BST
-int main(int argc, char** argv){
 
-  BSTNode *root = NULL;
-  root = insert("u", root);
-  root = insert("g", root);
-  root = insert("c", root);
-  root = insert("c", root);
-  root = insert("y", root);
-  root = insert("g", root);
-  root = insert("g", root);
-  root = insert("g", root);
-  root = insert("g", root);
-  root = insert("d", root);
-  //BSTNode** arr = treeToArr(root); // must be freed in main file
-  printBST(root);
-  heapNode** heap = BSTToHeap(root);
-  heapNode* min = deleteMin(heap);
-  min = deleteMin(heap);
-  heapNode* temp = (heapNode*)malloc(sizeof(heapNode));
-  temp->freq = 7;
-  temp->root = (BSTNode*)malloc(sizeof(BSTNode));
-  temp->root->freq = 7;
-  temp->root->token = " ";
-  insertHeap(heap, temp);
-  heapNode** heap = (heapNode**)malloc(6*sizeof(heapNode*));
-  int i;
-  for(i = 0; i < 6; i++){
-    heap[i] = (heapNode*)malloc(sizeof(heapNode));
-    heap[i]->root = (BSTNode*)malloc(sizeof(BSTNode));
-    heap[i]->root->left = NULL;
-    heap[i]->root->right = NULL;
-  }
-  heap[0]->freq = 5;
-  heap[0]->root->freq = 5;
-  heap[0]->root->token = "a";
-  heap[1]->freq = 9;
-  heap[1]->root->freq = 9;
-  heap[1]->root->token = "dog";
-  heap[2]->freq = 12;
-  heap[2]->root->freq = 12;
-  heap[2]->root->token = "cat";
-  heap[3]->freq = 13;
-  heap[3]->root->freq = 13;
-  heap[3]->root->token = "button";
-  heap[4]->freq = 16;
-  heap[4]->root->freq = 16;
-  heap[4]->root->token = "ball";
-  heap[5]->freq = 45;
-  heap[5]->root->freq = 45;
-  heap[5]->root->token = "and";
-
-  printHeap(heap);
-
-  printf("Encoding:\n");
-  huffEncode(heap);
-  printHuff(heap[0]);
-  //printBST(heap[0]->root);
-
-  //free(arr);
-  free(heap);
-
-  return 0;
-}
-*/
-
-// REAL MAIN
 int main(int argc, char** argv){
   // min args - ./file -b <path>, max args - ./file -R -d <path> <codebook>
   if(argc < 3 || argc > 5){
@@ -127,10 +60,10 @@ int main(int argc, char** argv){
     }
   }
   printf("OpFlag: %c, RFlag: %d\n", opFlag, r_flag);
-  /*
+
   // If recursive flag set, then open given directory and apply operation to each file
   if(r_flag){
-    DIR* currentDir = openDir(path);
+    DIR* currentDir = opendir(path);
     if(currentDir == NULL){
       printf("Error: invalid path\n");
       return -1;
@@ -152,10 +85,10 @@ int main(int argc, char** argv){
   // If r not set, just do once
   else{
     doOp();
-  }*/
-  //if(opFlag = 'b'){
-  //  buildCodebook(finalBST);
-  //}
+  }
+  if(opFlag == 'b'){
+    buildCodebook(finalBST);
+  }
 
   finalBST = addToBook(path, finalBST);
   buildCodebook(finalBST);
