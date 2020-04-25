5cc63424d10d6e736c7581082b1b7a4d8b9aa1bf
Matthew Notaro <matthew.y.notaro@gmail.com>
2020-04-25 00:32:47 -0400

    formatting

--- AsstLast/WTFserver.c
+++ AsstLast/WTFserver.c
@@ -9,7 +9,7 @@
 
 int connectToClient();
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	return 0;
 }
@@ -17,41 +17,40 @@ int main(int argc, char** argv)
 int connectToClient()
 {
 	char buffer[256];
-  int sockfd, bindSocket, listen, newsockfd;
-  struct sockaddr_in serverAddressInfo;
+	int sockfd, bindSocket, listen, newsockfd;
+	struct sockaddr_in serverAddressInfo;
 	struct sockaddr_in clientAddressInfo;
-    
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if(sockfd < 0)
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0)
 		printf("ERROR: Socket does not exist.\n");
- 
-  bzero(&serverAddressInfo, sizeof(serverAddressInfo));
- 
-  serverAddressInfo.sin_family = AF_INET;
-  serverAddressInfo.sin_addr.s_addr = htons(INADDR_ANY);
-  serverAddressInfo.sin_port = htons(PORT);
-    
-  bindSocket = bind(sockfd, (struct sockaddr *) &serverAddressInfo, sizeof(serverAddressInfo));
-	
-	if(bindSocket < 0)
+
+	bzero(&serverAddressInfo, sizeof(serverAddressInfo));
+
+	serverAddressInfo.sin_family = AF_INET;
+	serverAddressInfo.sin_addr.s_addr = htons(INADDR_ANY);
+	serverAddressInfo.sin_port = htons(PORT);
+
+	bindSocket = bind(sockfd, (struct sockaddr *)&serverAddressInfo, sizeof(serverAddressInfo));
+
+	if (bindSocket < 0)
 		printf("ERROR: Could not bind.\n");
- 
-  listen = listen(sockfd, 0);
-	
-	if(listen < 0)
+
+	listen = listen(sockfd, 0);
+
+	if (listen < 0)
 		printf("ERROR: Could not listen.\n");
- 
+
 	// keep running until listening stops
-	while(1)
+	while (1)
 	{
-		newsockfd = accept(sockfd, (struct sockaddr*) &clientAddressInfo, sizeof(clientAddressInfo));
-		
-		if(cxnfd < 0)
+		newsockfd = accept(sockfd, (struct sockaddr *)&clientAddressInfo, sizeof(clientAddressInfo));
+
+		if (cxnfd < 0)
 			printf("ERROR: Socket not accepted.\n");
-			
+
 		//pthread_create
 		//pthread_join
-		
 	}
 }
